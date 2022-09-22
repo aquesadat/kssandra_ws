f39c0e990b0c0384ddcf6fccdb4a4d12a59cc3a9
aquesada <aquesadatello@gmail.com>
2022-09-22 07:21:36 +0200

    Adding log traces

---
+++ src/test/java/com/kssandra/ksd_ws/controller/PredictionControllerTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.kssandra.ksd_ws.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author aquesada
+ *
+ */
+class PredictionControllerTest {
+
+	/**
+	 * Test method for {@link com.kssandra.ksd_ws.controller.PredictionController#getIntraDayData(com.kssandra.ksd_ws.request.IntradayPredictionRequest, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	void testGetIntraDayData() {
+		fail("Not yet implemented");
+	}
+
+}
