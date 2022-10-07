7f03dce241039a5aa2082c086a85c38dd7d09c7a
aquesada <aquesadatello@gmail.com>
2022-10-07 20:07:47 +0200

    Set dependency projects as libraries

--- src/main/java/com/kssandra/ksd_ws/KsdWsApplication.java
+++ src/main/java/com/kssandra/ksd_ws/KsdWsApplication.java
@@ -2,10 +2,14 @@ package com.kssandra.ksd_ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({ "com.kssandra" })
+@EntityScan("com.kssandra.ksd_persistence.domain")
+@EnableJpaRepositories("com.kssandra.ksd_persistence.repository")
 public class KsdWsApplication {
 
 	public static void main(String[] args) {
